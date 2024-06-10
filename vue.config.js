c26b50e8128e97a1b8cd8a5f1464419d135f9fdd
Catalin Rusnac <ctlnrsnc@gmail.com>
2024-06-10 03:11:23 +0200

    env fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// vue.config.js
+console.log('process.env.DEPLOY_TARGET:', process.env.DEPLOY_TARGET)
 module.exports = {
   publicPath: process.env.DEPLOY_TARGET === 'local' ? '/' : '/replifactory_simulate/',
   outputDir: 'dist',
