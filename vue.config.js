691e30cfab5a24553acd22ae09a5244db181ba71
Catalin Rusnac <ctlnrsnc@gmail.com>
2024-06-10 03:39:37 +0200

    env fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  publicPath: process.env.DEPLOY_TARGET === 'local' ? '/' : '/replifactory_simulate/',
+  module.exports = {
+  publicPath: '/replifactory_simulate/',
   outputDir: 'dist',
   devServer: {
     hot: true, // Ensure HMR is active
